1e755672e66f5f53cb55ee7c4418a47654603100
Rakib Hasan <rakibhasan1030@gmail.com>
2022-03-12 17:35:30 +0600

    Face Detection Update!

--- app/src/main/java/rakib/hasan/mlkitvision/MainActivity.java
+++ app/src/main/java/rakib/hasan/mlkitvision/MainActivity.java
@@ -1,14 +1,207 @@
 package rakib.hasan.mlkitvision;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.PointF;
+import android.graphics.Rect;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.Face;
+import com.google.mlkit.vision.face.FaceDetection;
+import com.google.mlkit.vision.face.FaceDetector;
+import com.google.mlkit.vision.face.FaceDetectorOptions;
+import com.google.mlkit.vision.face.FaceLandmark;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import rakib.hasan.mlkitvision.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity{
+    public static String TAG = MainActivity.class.getSimpleName();
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        /*-------  Status Color Code To Change  --------*/
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = this.getWindow();
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+            window.setStatusBarColor(this.getResources().getColor(R.color.white));
+        }
+
+        binding.activityMainAvatarChangeBtn.setOnClickListener(v -> {
+            Dexter.withContext(getApplicationContext())
+                    .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    .withListener(new PermissionListener() {
+                        @Override
+                        public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                            activityResultGalleryLauncher.launch("image/*");
+                        }
+                        @Override
+                        public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+
+                        }
+
+                        @Override
+                        public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                            permissionToken.continuePermissionRequest();
+                        }
+                    }).check();
+        });
+
     }
+
+    ActivityResultLauncher<String> activityResultGalleryLauncher = registerForActivityResult(new ActivityResultContracts.GetContent(),
+            new ActivityResultCallback<Uri>() {
+                @Override
+                public void onActivityResult(Uri result) {
+                    try {
+                        InputStream inputStream = getContentResolver().openInputStream(result);
+                        Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                        Bitmap b = Bitmap.createScaledBitmap(bitmap, 512, 512, true);
+                        binding.activityMainAvatarIv.setImageBitmap(b);
+
+                        InputImage image = imageFromBitmap(bitmap);
+                        detectFaces(image);
+
+
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+    public byte[] getFileDataFromDrawable(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 80, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+
+    private InputImage imageFromBitmap(Bitmap bitmap) {
+        int rotationDegree = 0;
+        // [START image_from_bitmap]
+        return InputImage.fromBitmap(bitmap, rotationDegree);
+        // [END image_from_bitmap]
+    }
+
+
+    private void detectFaces(InputImage image) {
+        // [START set_detector_options]
+        FaceDetectorOptions options =
+                new FaceDetectorOptions.Builder()
+                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
+                        .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_ALL)
+                        .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_ALL)
+                        .setMinFaceSize(0.15f)
+                        .enableTracking()
+                        .build();
+        // [END set_detector_options]
+
+        // [START get_detector]
+        FaceDetector detector = FaceDetection.getClient(options);
+        // Or use the default options:
+        // FaceDetector detector = FaceDetection.getClient();
+        // [END get_detector]
+
+        // [START run_detector]
+        Task<List<Face>> result =
+                detector.process(image)
+                        .addOnSuccessListener(
+                                new OnSuccessListener<List<Face>>() {
+                                    @Override
+                                    public void onSuccess(List<Face> faces) {
+                                        // Task completed successfully
+                                        // [START_EXCLUDE]
+                                        // [START get_face_info]
+
+                                        String facesInfo = null;
+
+                                        for (Face face : faces) {
+                                            Rect bounds = face.getBoundingBox();
+                                            float rotY = face.getHeadEulerAngleY();  // Head is rotated to the right rotY degrees
+                                            float rotZ = face.getHeadEulerAngleZ();  // Head is tilted sideways rotZ degrees
+
+                                            Log.v(TAG, "List of faces : " + faces.size() + "   |||   "+ face.toString());
+
+                                            facesInfo = facesInfo + face.toString() + "\n" + "\n" + "\n";
+
+                                            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                                            // nose available):
+                                            FaceLandmark leftEar = face.getLandmark(FaceLandmark.LEFT_EAR);
+                                            if (leftEar != null) {
+                                                PointF leftEarPos = leftEar.getPosition();
+                                            }
+
+                                            // If classification was enabled:
+                                            if (face.getSmilingProbability() != null) {
+                                                float smileProb = face.getSmilingProbability();
+                                            }
+                                            if (face.getRightEyeOpenProbability() != null) {
+                                                float rightEyeOpenProb = face.getRightEyeOpenProbability();
+                                            }
+
+                                            // If face tracking was enabled:
+                                            if (face.getTrackingId() != null) {
+                                                int id = face.getTrackingId();
+                                            }
+                                        }
+                                        binding.activityMainFaceDetectionInfoTv.setText(facesInfo);
+                                        binding.activityMainFaceDetectionInfoTv.setMovementMethod(new ScrollingMovementMethod());
+
+                                        // [END get_face_info]
+                                        // [END_EXCLUDE]
+                                    }
+                                })
+                        .addOnFailureListener(
+                                new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        // Task failed with an exception
+                                        Log.v(TAG, "Exception : "  + e.getMessage());
+                                    }
+                                });
+        // [END run_detector]
+    }
+
+
+
 }
